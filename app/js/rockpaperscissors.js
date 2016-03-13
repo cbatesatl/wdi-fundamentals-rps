7689fcd5e849cc201fdc7a10b965a7290c5b6701
Christopher Bates <christopherbates@christophersmbp.attlocal.net>
2016-03-13 18:04:32 -0400

    added move expressions

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,22 +25,29 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return if (move == undefined) {
+  console.log('No definition found');
+  move = `getInput()`;
+}
+move;
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return if (move == undefined) {
+  console.log('No definition found');
+  move = `randomPlay()`;
+}
+move  ;
 }
 
 function getWinner(playerMove,computerMove) {
     var winner;
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.    /* YOUR CODE HERE */
     return winner;
 }
 
